54d0062e68f57569408627a4bdfec63abcfe6a4b
Mathis <matis.cros@telecom-paris.fr>
2025-02-18 11:25:52 +0100

    ajout de la contrainte de normalisation sur l'ex3

--- Exercises/ex3_cvxpy.py
+++ Exercises/ex3_cvxpy.py
@@ -2,7 +2,7 @@ import cvxpy as cp
 
 Y = cp.Variable((3,3))
 
-constraints = [Y >> 0, Y[1,1] - Y[0, 1] == 0, -Y[2,2] + Y[0,2] + 0.25*Y[0,0] >= 0]
+constraints = [Y >> 0, Y[1,1] - Y[0, 1] == 0, -Y[2,2] + Y[0,2] + 0.25*Y[0,0] >= 0, Y[0, 0] == 1, Y == Y.T]
 
 obj = cp.Maximize(Y[1,2] - Y[2,1])
 
