4e0319ca5b0f5897c8ec35d93817bc9bc71d6ab1
Mathis <mathis.cros@telecom-paris.fr>
2025-02-21 13:36:21 +0100

    adding attribute of constraint class

--- src/constraints.py
+++ src/constraints.py
@@ -1,3 +1,6 @@
-class Constraints:
+class Constraint:
+    def __init__(self, equal_constraint, polynom):
+        self.equal_constraint = equal_constraint    # is the constraint an equality or not ?
+        self.polynom = polynom      # the constraint has the form p >= 0 or p = 0
 
-    def __init__(self): ...
+
