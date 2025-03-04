db122b275506de294cbc65ce02d7d60e87758b14
Mathis <mathis.cros@telecom-paris.fr>
2025-03-04 09:18:37 +0100

    adding of the function needed_monomials in momentmatrix.py

--- src/momentmatrix.py
+++ src/momentmatrix.py
@@ -2,6 +2,11 @@
 from monomial import *
 from typing import List
 
+# return a list of monomials without the monom that lead the equality rules 
+# ex: needed_monomials([x, x**2], {x : ...}) = [x**2]
+def needed_monomials(monomials, rules):
+        needed_mono = [monomial for monomial in monomials if monomial not in rules.keys()]
+        return needed_mono
 
 class MomentMatrix:
 
@@ -12,3 +17,5 @@ class MomentMatrix:
     def __repr__(self): ...
 
     def _method1(self): ...
+
+    
