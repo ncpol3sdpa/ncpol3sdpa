c3b3793ecdf84e1a3a1b0474ccea03f79b0ecd33
Thomas Debernardi <thomas.debernardi@telecom-paris.fr>
2025-03-04 10:47:38 +0000

    Update file momentmatrix.py

--- src/momentmatrix.py
+++ src/momentmatrix.py
@@ -9,7 +9,8 @@ def needed_monomials(monomials, rules):
         return needed_mono
 
 # 
-def create_matrix(L):
+def create_matrix(L):                           # L list of all the monomials used in the moment matrix
+                                                # this list goes to the degree k of the moment matrix
     n = len(L)
     M = []
     for i in range (n):
@@ -19,6 +20,18 @@ def create_matrix(L):
     for i in range(n):
         for j in range(n):
             M[i][j] = L[i] * L[j]
+    return M
+
+def create_matrix_of_constraints(L,Q,q):      # L list of all the monomials used in the moment matrix
+                                              # Q polynome of constraints Q(X,Y) < q
+    n = len(L)
+    M = []
+    for i in range (n):
+        P = [0] * n
+        M.append(P) 
+    for i in range(n):
+        for j in range(n):
+            M[i][j] = (q-Q)(L[i] * L[j]) 
     return M
 
 class MomentMatrix:
