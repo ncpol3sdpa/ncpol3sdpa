bec4edfdc317a01ea30927a56f0cd884ca89abb0
Thomas Debernardi <thomas.debernardi@telecom-paris.fr>
2025-03-04 08:38:34 +0000

    Update file momentmatrix.py

--- src/momentmatrix.py
+++ src/momentmatrix.py
@@ -8,9 +8,24 @@ def needed_monomials(monomials, rules):
         needed_mono = [monomial for monomial in monomials if monomial not in rules.keys()]
         return needed_mono
 
+def create_matrix(L):
+    n = len(L)
+    M = []
+    for i in range (n):
+        P = [0] * n
+        M.append(P)   
+        
+    for i in range(n):
+        for j in range(n):
+            M[i][j] = L[i] * L[j]
+            if M[i][j] in rules:
+                M[i][j] = rules(M[i][j])
+    return M
+
 class MomentMatrix:
 
     def __init__(self): ...
+
 
     def __mul__(self, other): ...
 
