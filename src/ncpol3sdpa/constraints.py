cf7f74af9854ceb6bdba8b67b27acdb98449fa22
Mathis Cros <mathis.cros@telecom-paris.fr>
2025-03-12 10:42:04 +0100

    correction if tab issues in constraints.py

--- src/ncpol3sdpa/constraints.py
+++ src/ncpol3sdpa/constraints.py
@@ -2,11 +2,10 @@
 
 class Constraint:
     def __init__(self, equal_constraint, polynom):
-    	""" 
-    	equal_constraint : boolean indicating whether the constraint is >= or ==
-    	polynom : sympy polynom
-    	"""
-
+        """ 
+        equal_constraint : boolean indicating whether the constraint is >= or ==
+        polynom : sympy polynom
+        """
         self.equal_constraint = equal_constraint  # is the constraint an equality or not ?
         self.polynom = polynom  # the constraint has the form p >= 0 or p = 0
         
