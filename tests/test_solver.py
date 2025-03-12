a2ca5b11a487a8a2a8906a5c5a12a111bff5a046
Mathis Cros <mathis.cros@telecom-paris.fr>
2025-03-12 11:27:58 +0100

    adding test for the communication with solver

---
+++ tests/test_solver.py
@@ -0,0 +1,18 @@
+from ncpol3sdpa.solver import *
+from sympy import symbols 
+
+def test_1():
+    y1, y2, y3, y4, y5, y6 = symbols("y1 y2 y3 y4 y5 y6")
+    p_obj = 2*y5 
+    constraint1 = [[y1-1]]
+    constraint2 = [[y4-y2]]
+    constraint3 = [[-y6+y3+0.25*y1]]
+    moment_matrix = [
+        [y1, y2, y3], 
+        [y2, y4, y5],
+        [y3, y5, y6]
+    ]
+    constraint_zero = [constraint1, constraint2]
+    constraint_positiv = [constraint3]
+    assert(abs(Solver.solve(p_obj, 1, moment_matrix, constraint_positiv, constraint_zero) - 2.414) <= 0.1)
+
