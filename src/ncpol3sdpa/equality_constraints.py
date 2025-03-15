907270cc2cfced8e472a655344f880f0d42cd5a7
Mathis Cros <mathis.cros@telecom-paris.fr>
2025-03-15 16:34:25 +0100

    debugging apply_rule + tests forapply_rule

--- src/ncpol3sdpa/equality_constraints.py
+++ src/ncpol3sdpa/equality_constraints.py
@@ -2,12 +2,15 @@
 # exemple : rule_of_constraint(x²-x-1=0) = (x², x+1) because the rule is x² -> x+1
 from sympy.ntheory import qs
 from sympy import rem
+from sympy import poly
+
 
 def rule_of_constraint(constraint):
-    polynom = constraint.polynom.terms()  # express polynom as a list of monom
+
+    polynom = poly(constraint.polynom).terms()  # express polynom as a list of monom
 
     max_degree = (
-        constraint.polynom.total_degree()
+        poly(constraint.polynom).total_degree()
     )  # degree of the polynom seen as a multivariable polynom
 
     leader_monomials = [
@@ -17,9 +20,9 @@ def rule_of_constraint(constraint):
     leader_monomial = leader_monomials[0]
 
     leader_monomial_expressed = 1  # leader_monom expressed with variables
-    for monom_index in range(len(constraint.polynom.gens)):
+    for monom_index in range(len(poly(constraint.polynom).gens)):
         leader_monomial_expressed *= (
-            constraint.polynom.gens[monom_index] ** leader_monomial[0][monom_index]
+            poly(constraint.polynom).gens[monom_index] ** leader_monomial[0][monom_index]
         )
 
     polynom_without_leader = constraint.polynom  # rewriting the constraint as a rule
@@ -27,7 +30,7 @@ def rule_of_constraint(constraint):
     polynom_without_leader -= leader_monomial_expressed
     polynom_without_leader *= -1
 
-    return [leader_monomial_expressed, polynom_without_leader]
+    return [leader_monomial_expressed.as_expr(), polynom_without_leader.as_expr()]
 
 
 # return the hashtbl rules that represent a list of constraint, constraints
@@ -40,12 +43,9 @@ def rules_of_constraints(constraints):
     return rules
 
 def apply_rule(monom, rules):
-    print("RULES 2", rules)
     for key in rules.keys():
-        if rem(key, monom) == 0:
-            monom = monom*rules[key]/key 
-            monom = apply_rule(monom, rules) 
-            return monom 
+        if rem(monom, key) == 0:
+            return apply_rule(monom*rules[key]/key, rules) 
     return monom
 
 def solve_equality_constraints(): ...
--- tests/test_equality_constraint.py
+++ tests/test_equality_constraint.py
@@ -1,16 +1,19 @@
 import sympy as sp
 from ncpol3sdpa.constraints import Constraint
-from ncpol3sdpa.equality_constraints import rule_of_constraint, rules_of_constraints
+from ncpol3sdpa.equality_constraints import rule_of_constraint, rules_of_constraints, apply_rule
 
 
 def test_rule_of_constraint():
     x, y = sp.symbols("x y")
     pol1 = sp.poly(x**2 - x - 1)
     pol2 = sp.poly(3 * x * y**2 - 6 * x**2 + 12 * y)
+    pol3 = sp.poly(x-2)
     c1 = Constraint(True, pol1)
     c2 = Constraint(True, pol2)
+    c3 = Constraint(True, pol3)
     assert rule_of_constraint(c1) == [x**2, x + 1]
     assert rule_of_constraint(c2) == [x * y**2, 2 * x**2 - 4 * y]
+    assert rule_of_constraint(c3) == [x, 2]
 
 
 def test_rules_of_constraints():
@@ -20,3 +23,14 @@ def test_rules_of_constraints():
     c1 = Constraint(True, pol1)
     c2 = Constraint(True, pol2)
     assert rules_of_constraints([c1, c2]) == {x**2: x + 1, x * y**2: 2 * x**2 - 4 * y}
+
+def test_apply_rule():
+    x, y = sp.symbols("x y")
+    rules = {x**2 : x} 
+    p1 = x*y 
+    p2 = x**2*y 
+    p3 = x**6*y**2 * 5 
+    assert(apply_rule(p1, rules) == x*y)
+    assert(apply_rule(p2, rules) == x*y)
+    assert(apply_rule(p3, rules) == 5*x*y**2)
+
