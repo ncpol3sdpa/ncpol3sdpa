e5d1af651c6059be85d049d982bc7624bc8225fc
Nazar Chernyak <nazar.chernyak@telecom-paris.fr>
2025-03-23 11:22:04 +0100

    Add AlgebraSDP class to moment_matrix.py
    This class will encapsulate all algebraic/symbolic transformation of the polynomials.

--- src/ncpol3sdpa/momentmatrix.py
+++ src/ncpol3sdpa/momentmatrix.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 from typing import List, Tuple, Dict, Any
 import sympy as sp
 from ncpol3sdpa.rules import apply_rule, apply_rule_to_polynom
-
+from ncpol3sdpa.monomial import generate_monomials_commutative
+from ncpol3sdpa.constraints import Constraint
 
 def needed_monomials(monomials : List[sp.Poly], rules : Dict[sp.Poly, Any]) -> List[sp.Poly]:
     """Filter the monomials according to the rules"""
@@ -20,18 +21,41 @@ def create_moment_matrix(monomials : List[sp.Poly]) -> List[List[sp.Poly]]:
         for y in monomials
     ]
 
-def create_constraints_matrix(
+def create_moment_matrix_commutative(
+    monomials : List[sp.Poly], 
+    substitution_rules : Dict[sp.Poly, Any]
+) -> List[List[sp.Poly]]:
+    """Create the moment matrix of the monomials for the commutative case"""
+
+    matrix_size = len(monomials)
+    return [[apply_rule(monomials[i] * monomials[j], substitution_rules) \
+            for j in range(i+1)] for i in range(matrix_size)]
+
+def create_constraint_matrix(
     monomials : List[sp.Poly],
     Q : sp.Poly,
     q : sp.Poly
 ) -> List[List[sp.Poly]]:
     """Create the matrix of constraints
-    The constraints are of the form Q(X,Y) < q
+    The constraints are of the form Q(X,Y) >= 0
     """
     return [
         [ (q - Q) * (x * y) for x in monomials ]
         for y in monomials
     ]
+
+def create_constraints_matrix_commutative(
+    monomials : List[sp.Poly], 
+    constraint_polynomial : sp.Poly,
+    rules : Dict[sp.Poly, Any] 
+) -> List[List[sp.Poly]]:
+    """Create the matrix of constraints
+    The constraints are of the form constraint_polynomial >= 0
+    """
+
+    n = len(monomials)
+    return [[apply_rule_to_polynom(monomials[i] * monomials[j] * constraint_polynomial, rules) \
+            for j in range(i+1)] for i in range(n)]
 
 
 def create_moment_matrix_cvxpy(
@@ -43,6 +67,7 @@ def create_moment_matrix_cvxpy(
     n : int = len(monomials)
     index_var : int = 0
     moment_matrix : List[List[sp.Poly]] = [[0 for _ in range(n)] for _ in range(n)]
+    variable_of_monomial = {}
 
     for i, monom1 in enumerate(monomials):
         for j, monom2 in enumerate(monomials):
@@ -75,7 +100,6 @@ def create_constraints_matrix_cvxpy(
                 
     return matrix
 
-
 class MomentMatrix:
     def __init__(self, polynom : sp.Poly, monomials : List[sp.Poly]) -> None:
         self.optimized = None
@@ -89,3 +113,46 @@ class MomentMatrix:
 
     def _method1(self) -> None:
         raise NotImplementedError
+
+class AlgebraSDP:
+    def __init__(self, objective : sp.Poly, relaxation_order : int, substitution_rules : Dict[sp.Poly, sp.Poly]) -> None:
+        """ Construct the symbolic Moment Matrices and soundings data structures. Works for the commutative case """ 
+        self.objective = objective
+        self.relaxation_order = relaxation_order
+        self.substitution_rules = substitution_rules
+        self.monomials : List[sp.Poly] = needed_monomials( \
+                generate_monomials_commutative(objective.free_symbols, relaxation_order), \
+                substitution_rules\
+        )
+
+        # In the commutative case, the moment matrix is symmetric
+        self.symbolic_moment_matrix = create_moment_matrix_commutative(self.monomials, self.substitution_rules)
+        matrix_size = len (self.symbolic_moment_matrix)
+
+        # equivalence classes of equal coefficients
+        self.monomial_to_positions : Dict[sp.Poly,List[Tuple[int, int]]]= {}
+        for i in range(matrix_size):
+            for j in range(i+1):
+                monomial = self.symbolic_moment_matrix[i][j]
+                if monomial in self.monomial_to_positions.keys():
+                    self.monomial_to_positions[monomial].append((i,j))
+                else:
+                    self.monomial_to_positions[monomial] = [(i,j)]
+
+        # This is the positive semi-definite matrices in the sdp 
+        #                                       v TODO Should this be a new type? discuss
+        self.constraint_moment_matrices : List[ List[List[sp.poly]] ] = []
+        # List of polynomials that equal 0
+        self.equality_constraints : List[sp.poly] = []
+    
+    def add_constraint(self, constraint : Constraint) -> None:
+        if constraint.is_equality_constraint:
+            self.equality_constraints.append(constraint.polynom)
+        else:
+            #inequality constraint
+            self.constraint_moment_matrices.append( \
+                create_moment_matrix_commutative(self.monomials, self.substitution_rules))
+
+    def add_constraints(self, constraints : List[Constraint]) -> None:
+        for constraint in constraints:
+            self.add_constraint(constraint)
