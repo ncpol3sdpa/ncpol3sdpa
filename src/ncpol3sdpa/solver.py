82ad0d85c4271c5944b2990edcb14f706111a8ab
Nazar Chernyak <nazar.chernyak@telecom-paris.fr>
2025-03-24 15:19:25 +0100

    Fix bug where the moment matrix was created twice in cvxpy solver

--- src/ncpol3sdpa/solver.py
+++ src/ncpol3sdpa/solver.py
@@ -6,29 +6,26 @@ from sympy import Symbol
 import ncpol3sdpa.semidefinite_program_repr as sdp_repr
 
 def cvxpy_dot_prod(c : Any, x : Any) -> Any:
-    return cvxpy.sum(cvxpy.multiply(c, x))
+    return cvxpy.trace(c @ x)
+    # return cvxpy.sum(cvxpy.multiply(c, x))
 
 class Solver:
 
     @classmethod
     def solve_cvxpy(self, problem : sdp_repr.ProblemSDP) -> Any:
         """Solve the SDP problem with cvxpy"""
+        # Variables
+        sdp_vars = [cvxpy.Variable((size, size), PSD=True) \
+                          for size in problem.variable_sizes]
 
-        moment_matrix_size = problem.variable_sizes[problem.MOMENT_MATRIX_VAR_NUM]
-        G = cvxpy.Variable((moment_matrix_size, moment_matrix_size), PSD=True)
-        constraints = [G[0,0] == 1]
-
-        
         # Moment matrix structure
+        G = sdp_vars[problem.MOMENT_MATRIX_VAR_NUM]
+        constraints = [G[0,0] == 1]
         for eq_class in problem.moment_matrix.eq_classes:
             assert len(eq_class) > 0
             (i,j) = eq_class.pop() 
             for (x,y) in eq_class:
                 constraints.append(G[i,j] == G[x,y])
-        
-        # Variables
-        sdp_vars = [cvxpy.Variable((size, size), PSD=True) \
-                    for size in problem.variable_sizes]
 
         # Constraints
         for constraint in problem.constraints:
@@ -38,6 +35,9 @@ class Solver:
                 expression += cvxpy_dot_prod(matrix, sdp_vars[var_num])
             constraints.append(0 == expression)
 
+        print("SOLVER constraints:")
+        print(constraints)
+        
         # tr(A.T x G)
         objective = cvxpy.Maximize(cvxpy_dot_prod(problem.objective, G))
         
