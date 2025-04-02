2efd9c81d9c9fddf45c82643f7b7a8d692be1342
Mathis Cros <mathis.cros@telecom-paris.fr>
2025-04-02 15:27:10 +0200

    update apply_rule and apply_rule_to_polynomial for non commutative monomial and polynomial

--- src/ncpol3sdpa/rules.py
+++ src/ncpol3sdpa/rules.py
@@ -1,7 +1,10 @@
 from __future__ import annotations
-from typing import List, Tuple, Dict
+
+from typing import Dict, List, Tuple
+
+from sympy import Expr, S, poly, rem
+
 from ncpol3sdpa.constraints import Constraint
-from sympy import rem, poly, Expr, S
 
 
 class Rule:
@@ -38,21 +41,35 @@ class Rule:
         return dict([Rule._of_constraint(constraint) for constraint in constraints])
 
 
-def apply_rule(monomial: Expr, rules: Dict[Expr, Expr]) -> Expr:
+def apply_rule(
+    monomial: Expr, rules: Dict[Expr, Expr], commutative: bool = True
+) -> Expr:
     """Apply a rule to a monomial"""
+    if commutative:
+        for key in rules.keys():
+            if rem(monomial, key) == 0:
+                return apply_rule(monomial * rules[key] / key, rules)
+        return monomial
+    else:
+        for key in rules.keys():
+            to_change_monomial = monomial.as_ordered_factors()
+            rule_monomial = key.as_ordered_factors()
+            for i in range(len(to_change_monomial) - len(rule_monomial) + 1):
+                if to_change_monomial[i : i + len(rule_monomial)] == rule_monomial:
+                    return apply_rule(
+                        to_change_monomial[: max(0, i - 1)]
+                        * rules[key]
+                        * to_change_monomial[(i + len(rule_monomial)) :]
+                    )
+        return monomial
 
-    for key in rules.keys():
-        if rem(monomial, key) == 0:
-            return apply_rule(monomial * rules[key] / key, rules)
-    return monomial
 
-
-def apply_rule_to_polynomial(polynomial: Expr, rules: Dict[Expr, Expr]) -> Expr:
+def apply_rule_to_polynomial(polynomial: Expr, rules: Dict[Expr, Expr], commutative: bool = True) -> Expr:
     """Apply a rule to a polynomial"""
 
     poly_dict: Dict[Expr, int] = polynomial.as_coefficients_dict()  # type: ignore
 
     res: Expr = S.Zero
     for monomial, coef in poly_dict.items():
-        res += coef * apply_rule(monomial, rules)
+        res += coef * apply_rule(monomial, rules, commutative)
     return res
