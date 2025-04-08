4584480769a693a48e069a6971d76b6ba3871a83
Thomas Debernardi <thomas.debernardi@telecom-paris.fr>
2025-04-08 11:16:09 +0200

    solve the error

--- tests/test_sos.py
+++ tests/test_sos.py
@@ -12,18 +12,40 @@ def test_dual_constraints_cvxpy() -> None:
     dual_constraints = Sos.dual_constraints_cvxpy(p)
 
     x = cp.Variable()
-    constraints = [x>=0 , x == 1]
+    constraints = [ x == 1, x>=0 ]
     obj = cp.Maximize (-x)
     prob = cp.Problem(obj,constraints)
     prob.solve()
-    
+
     cvxpy_dual_constraints = []
     for constraint in constraints:
         cvxpy_dual_constraints.add(constraint.dual_value) 
     
     assert (dual_constraints == cvxpy_dual_constraints)
 
+def test_dual_constraints_cvxpy_2() -> None:
+    # Arithmetic - Geometric inequality
+    moment_matrix = MomentMatrixSDP(2, [[(0, 0), (1, 1)], [(1, 0)]])
+    # [ 1 a ]
+    # [ a 1 ]
+    p = ProblemSDP(moment_matrix, np.array([[0, -1], [-1, 0]]))
+    # maximize -2a, optimal for a = -1, and objective = 2
+    # x2 -2axy + y2 >= 0, because SDP
 
+    dual_constraints = Sos.dual_constraints_cvxpy(p)
+    
+    x = cp.Variable()
+    y = cp.Variable()
+    constraints = [ x + y == 1,x - y >= 1]
+    obj = cp.Minimize ((x-y)**2)
+    prob = cp.Problem(obj,constraints)
+    prob.solve()
+
+    cvxpy_dual_constraints = []
+    for constraint in constraints:
+        cvxpy_dual_constraints.append(constraint.dual_value) 
+    
+    assert (dual_constraints == cvxpy_dual_constraints)
 
 
 
