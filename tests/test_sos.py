8997b1bec28437b0da70f8a1802616cdcbc78418
Thomas Debernardi <thomas.debernardi@telecom-paris.fr>
2025-04-08 10:32:28 +0200

    début de l'implémentation du problème SOS + test qui va avec

---
+++ tests/test_sos.py
@@ -0,0 +1,30 @@
+from ncpol3sdpa.solver import Solver
+from ncpol3sdpa.sos import Sos
+from ncpol3sdpa.sdp_repr import MomentMatrixSDP, ProblemSDP
+import numpy as np
+import cvxpy as cp
+# from typing import List
+
+
+def test_dual_constraints_cvxpy() -> None:
+    moment_matrix = MomentMatrixSDP(1, [[(0, 0)]])
+    p = ProblemSDP(moment_matrix, np.array([[-1]]))
+    dual_constraints = Sos.dual_constraints_cvxpy(p)
+
+    x = cp.Variable()
+    constraints = [x>=0 , x == 1]
+    obj = cp.Maximize (-x)
+    prob = cp.Problem(obj,constraints)
+    prob.solve()
+    
+    cvxpy_dual_constraints = []
+    for constraint in constraints:
+        cvxpy_dual_constraints.add(constraint.dual_value) 
+    
+    assert (dual_constraints == cvxpy_dual_constraints)
+
+
+
+
+
+    
