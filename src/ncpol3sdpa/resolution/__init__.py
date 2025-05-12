26ef2050ed49e1e1d255f2af83aaf91e4c06be51
Yann <ruellan.yann@laposte.net>
2025-05-12 17:19:49 +0200

    Defined the __init__

---
+++ src/ncpol3sdpa/resolution/__init__.py
@@ -0,0 +1,11 @@
+
+from .rules import Rule, apply_rule_to_polynomial
+from .constraints import Constraint
+from .algebra import AlgebraSDP
+
+__all__ = [
+    "Rule", 
+    "apply_rule_to_polynomial", 
+    "Constraint",
+    "AlgebraSDP"
+]
---
+++ src/ncpol3sdpa/sdp_repr/__init__.py
@@ -0,0 +1,14 @@
+
+from .sdp_repr import (
+    ProblemSDP,
+    EqConstraint,
+    MomentMatrixSDP,
+    complexSDP_to_realSDP
+)
+
+__all__ = [
+    "ProblemSDP",
+    "EqConstraint",
+    "complexSDP_to_realSDP",
+    "MomentMatrixSDP"
+]
