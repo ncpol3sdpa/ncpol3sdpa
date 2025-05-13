baca69d93aef6582f00a37cf18838611a18cfd78
Yann <ruellan.yann@laposte.net>
2025-05-13 09:43:14 +0200

    Refactor ProblemSDP class: encapsulate attributes, improve string representation, and add internal validation methods

--- src/ncpol3sdpa/sdp_repr/problem_SDP.py
+++ src/ncpol3sdpa/sdp_repr/problem_SDP.py
@@ -21,106 +21,16 @@ class ProblemSDP:
         objective: NDArray[np.float64] | NDArray[np.complex64],
     ) -> None:
         # The moment matrix should always be in position 0
-        self.MOMENT_MATRIX_VAR_NUM = 0
+        self.__MOMENT_MATRIX_VAR_NUM: int = 0
 
-        self.moment_matrix = moment_matrix
+        self.__moment_matrix: MomentMatrixSDP = moment_matrix
         # The number of elements in this list is the number of variables.
         # Each variable is a positive semi-definite matrix
-        self.variable_sizes = [moment_matrix.size]
-        self.objective: NDArray[np.complex64] | NDArray[np.float64] = objective
-        self.constraints: List[EqConstraint] = []
+        self.__variable_sizes: List[int] = [moment_matrix.size]
+        self.__objective: NDArray[np.complex64] | NDArray[np.float64] = objective
+        self.__constraints: List[EqConstraint] = []
+
         assert objective.shape == (moment_matrix.size, moment_matrix.size)
-
-    def add_symmetric_variable(self, matrix_size: int) -> None:
-        self.variable_sizes.append(matrix_size)
-
-    def eq_2_coefs_constraint(
-        self,
-        coef1: Tuple[int, int],
-        coef2: Tuple[int, int],
-        var_number: int,
-    ) -> EqConstraint:
-        x, y = coef2
-        i, j = coef1
-        assert coef1 != coef2
-        a = np.zeros(
-            shape=(self.variable_sizes[var_number], self.variable_sizes[var_number])
-        )
-        a[i][j] += 0.5
-        a[j][i] += 0.5
-        a[x][y] -= 0.5
-        a[y][x] -= 0.5
-
-        return EqConstraint([(var_number, a)])
-
-    def compile_moment_matrix_to_constraints(self) -> None:
-        """Converts the equivalence class representation of the moment matrix to low level linear
-        constraints. Equivalence classes are removed to prevent errors."""
-
-        for eq_class in self.moment_matrix.eq_classes:
-            assert len(eq_class) > 0
-            central_coef = eq_class.pop()
-            for coef in eq_class:
-                self.constraints.append(
-                    self.eq_2_coefs_constraint(
-                        central_coef, coef, var_number=self.MOMENT_MATRIX_VAR_NUM
-                    )
-                )
-            eq_class = []
-
-        # the structure of the equality constraints is star:
-        # s0= s1, s0 = s2, s0 = s3, ...
-        # TODO/Idea: What if it was a chain(s0 = s1, s1 = s2, s2 = s3, ...)
-        # or a balanced tree or a Union-Find structure?
-
-    # --- Validation ---
-    def is_constraint_verified(
-        self, constraint: EqConstraint, variable_instances: List[NDArray[np.float64]]
-    ) -> bool:
-        """Verify if a constraint is satisfied"""
-        sum = 0
-
-        for var_num, a_matrix in constraint.constraints:
-            sum += np.sum(np.multiply(a_matrix, variable_instances[var_num]))
-
-        return abs(sum) < EPSILON
-
-    def is_feasible(self, variable_instances: List[NDArray[np.float64]]) -> bool:
-        """Verify that a given point is a feasible point of the problem"""
-        for var_i in variable_instances:
-            if np.linalg.norm(var_i - var_i.T, 1) > EPSILON:
-                return False
-            elif not (
-                np.all(np.linalg.eigvals(var_i) >= -EPSILON)
-            ):  # positive semidefinite
-                return False
-
-        moment_matrix = variable_instances[self.MOMENT_MATRIX_VAR_NUM]
-
-        for eq_class in self.moment_matrix.eq_classes:
-            eq_class_i = iter(eq_class)
-            x, y = eq_class_i.__next__()
-            for i, j in eq_class_i:
-                if abs(moment_matrix[i][j] - moment_matrix[x][y]) > EPSILON:
-                    return False
-
-        for constraint in self.constraints:
-            self.is_constraint_verified(constraint, variable_instances)
-
-        return True
-
-    def calculate_objective(
-        self, variable_instances: List[NDArray[np.float64]]
-    ) -> np.float64:
-        """Calculate the objective function at a given point"""
-
-        moment_matrix: NDArray[np.float64] = variable_instances[
-            self.MOMENT_MATRIX_VAR_NUM
-        ]
-        res: np.float64 = np.sum(np.multiply(self.objective, moment_matrix))
-        assert isinstance(res, np.float64)
-
-        return res
 
     def __str__(self) -> str:
         s = (
@@ -140,7 +50,144 @@ class ProblemSDP:
 
         return s
 
-    # --- Conversion ---
+    # --- Getter and Setter methods ---
+
+    @property
+    def MOMENT_MATRIX_VAR_NUM(self) -> int:
+        """The number of the moment matrix variable"""
+        return self.__MOMENT_MATRIX_VAR_NUM
+
+    @MOMENT_MATRIX_VAR_NUM.setter
+    def MOMENT_MATRIX_VAR_NUM(self, value: int) -> None:
+        """Set the number of the moment matrix variable"""
+        assert value >= 0
+        self.__MOMENT_MATRIX_VAR_NUM = value
+
+    @property
+    def moment_matrix(self) -> MomentMatrixSDP:
+        """The moment matrix"""
+        return self.__moment_matrix
+
+    @property
+    def variable_sizes(self) -> List[int]:
+        """The sizes of the variables"""
+        return self.__variable_sizes
+
+    @variable_sizes.setter
+    def variable_sizes(self, value: List[int]) -> None:
+        """Set the sizes of the variables"""
+        self.__variable_sizes = value
+
+    @property
+    def objective(self) -> NDArray[np.complex64] | NDArray[np.float64]:
+        """The objective matrix"""
+        return self.__objective
+
+    @property
+    def constraints(self) -> List[EqConstraint]:
+        """The constraints of the SDP"""
+        return self.__constraints
+
+    # --- unused ---
+
+    # def _add_symmetric_variable(self, matrix_size: int) -> None:
+    #     self.variable_sizes.append(matrix_size)
+
+    # def _is_feasible(self, variable_instances: List[NDArray[np.float64]]) -> bool:
+    #     """Verify that a given point is a feasible point of the problem"""
+    #     for var_i in variable_instances:
+    #         if np.linalg.norm(var_i - var_i.T, 1) > EPSILON:
+    #             return False
+    #         elif not (
+    #             np.all(np.linalg.eigvals(var_i) >= -EPSILON)
+    #         ):  # positive semidefinite
+    #             return False
+
+    #     moment_matrix = variable_instances[self.MOMENT_MATRIX_VAR_NUM]
+
+    #     for eq_class in self.moment_matrix.eq_classes:
+    #         eq_class_i = iter(eq_class)
+    #         x, y = eq_class_i.__next__()
+    #         for i, j in eq_class_i:
+    #             if abs(moment_matrix[i][j] - moment_matrix[x][y]) > EPSILON:
+    #                 return False
+
+    #     for constraint in self.constraints:
+    #         self._is_constraint_verified(constraint, variable_instances)
+
+    #     return True
+
+    # def _calculate_objective(
+    #     self, variable_instances: List[NDArray[np.float64]]
+    # ) -> np.float64:
+    #     """Calculate the objective function at a given point"""
+
+    #     moment_matrix: NDArray[np.float64] = variable_instances[
+    #         self.MOMENT_MATRIX_VAR_NUM
+    #     ]
+    #     res: np.float64 = np.sum(np.multiply(self.objective, moment_matrix))
+    #     assert isinstance(res, np.float64)
+
+    #     return res
+
+    # --- internal functions ---
+
+    # inner function
+    def _eq_2_coefs_constraint(
+        self,
+        coef1: Tuple[int, int],
+        coef2: Tuple[int, int],
+        var_number: int,
+    ) -> EqConstraint:
+        x, y = coef2
+        i, j = coef1
+        assert coef1 != coef2
+        a = np.zeros(
+            shape=(self.variable_sizes[var_number], self.variable_sizes[var_number])
+        )
+        a[i][j] += 0.5
+        a[j][i] += 0.5
+        a[x][y] -= 0.5
+        a[y][x] -= 0.5
+
+        return EqConstraint([(var_number, a)])
+
+    # inner function
+    def _is_constraint_verified(
+        self, constraint: EqConstraint, variable_instances: List[NDArray[np.float64]]
+    ) -> bool:
+        """Verify if a constraint is satisfied"""
+        sum = 0
+
+        for var_num, a_matrix in constraint.constraints:
+            sum += np.sum(np.multiply(a_matrix, variable_instances[var_num]))
+
+        return abs(sum) < EPSILON
+
+    # --- methods ---
+
+    # use in mosek_solver
+    def compile_moment_matrix_to_constraints(self) -> None:
+        """Converts the equivalence class representation of the moment matrix to low level linear
+        constraints. Equivalence classes are removed to prevent errors."""
+
+        for eq_class in self.moment_matrix.eq_classes:
+            assert len(eq_class) > 0
+            central_coef = eq_class.pop()
+            for coef in eq_class:
+                self.constraints.append(
+                    self._eq_2_coefs_constraint(
+                        central_coef, coef, var_number=self.MOMENT_MATRIX_VAR_NUM
+                    )
+                )
+            eq_class = []
+
+        # the structure of the equality constraints is star:
+        # s0= s1, s0 = s2, s0 = s3, ...
+        # TODO/Idea: What if it was a chain(s0 = s1, s1 = s2, s2 = s3, ...)
+        # or a balanced tree or a Union-Find structure?
+
+    # use in problem.py
     def complex_to_realSDP(self) -> ProblemSDP:
         # creation of the real moment matrix [[Hr, -Hi], [Hi, Hr]]
         real_eq_classes = []
