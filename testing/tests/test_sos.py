5c472d94a2590f39a3efbcf7bbc32dcfcbb07d9f
Yann <ruellan.yann@laposte.net>
2025-06-25 17:10:50 +0200

    set max_examples to 10 in test_bounded_no_constraints

--- testing/tests/test_sos.py
+++ testing/tests/test_sos.py
@@ -77,7 +77,7 @@ def test_mosek_ilposed() -> None:
 # --------- Automatic property tests ---------
 
 
-@settings(deadline=2000)
+@settings(deadline=2000, max_examples=10)
 @given(
     sos_polynomials(
         three_symbols, expand=False, coefs=order_of_magnitude_floats, max_degree=1
