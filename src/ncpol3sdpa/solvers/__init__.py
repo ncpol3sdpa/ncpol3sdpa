1ded06ab49841f1ef40ee6ae24075eb34a8c2cf9
Yann <ruellan.yann@laposte.net>
2025-06-25 15:08:23 +0200

    Update init.py

--- src/ncpol3sdpa/solvers/__init__.py
+++ src/ncpol3sdpa/solvers/__init__.py
@@ -1,4 +1,7 @@
 from .solver import Solver
-from .solver_registry import AvailableSolvers, SolverRegistry
+# from .solver_registry import AvailableSolvers, SolverRegistry
+from .solver_factory import SolverFactory
 
-__all__ = ["AvailableSolvers", "Solver", "SolverRegistry"]
+# __all__ = ["AvailableSolvers", "Solver", "SolverRegistry"]
+
+__all__ = ["Solver", "SolverFactory"]
