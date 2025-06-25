4628735ab7f2d798d67e427bcfaa474fa62d094f
Yann <ruellan.yann@laposte.net>
2025-06-25 20:40:49 +0200

    Include SolverList (v2)

--- testing/tests/err.py
+++ testing/tests/err.py
@@ -1,15 +1,15 @@
-from ncpol3sdpa import Problem, Constraint, SolverFactory
+from ncpol3sdpa import Problem, Constraint, SolverList
 from sympy.abc import x, y
 
 def test_mosek() -> None:
     pb = Problem(-x*x-y)
     pb.add_constraint(Constraint.EqualityConstraint(y-1))
-    pb.solve(solver=SolverFactory.create_solver("mosek"))
+    pb.solve(solver=SolverList.MOSEK)
 
 def test_cvxpy() -> None:
     pb = Problem(-x*x-y)
     pb.add_constraint(Constraint.EqualityConstraint(y-1))
-    pb.solve(solver=SolverFactory.create_solver("cvxpy"))
+    pb.solve(solver=SolverList.CVXPY)
 
 if __name__ == "__main__":
     
